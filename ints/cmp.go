cf6d627e0494f920f020ce95627973678c52a2d4
XS <devbycm@gmail.com>
2019-03-17 14:40:17 +1100

    update

---
+++ ints/cmp.go
@@ -0,0 +1,24 @@
+package ints
+
+func maxOrMin(nums []int, max bool) int {
+	if len(nums) == 0 {
+		panic("input length zero")
+	}
+	most := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if max && nums[i] > most {
+			most = nums[i]
+		} else if !max && nums[i] < most {
+			most = nums[i]
+		}
+	}
+	return most
+}
+
+func Max(nums []int) int {
+	return maxOrMin(nums, true)
+}
+
+func Min(nums []int) int {
+	return maxOrMin(nums, true)
+}
